d9e71279ac245d399a274b7c57a3ec9bb1c0296d
shenjun <1024772494@qq.com>
2019-03-01 08:18:16 +0800

    OK

--- haoze-medical-admin/src/main/java/com/haoze/admin/service/impl/FastMenuServcieImpl.java
+++ haoze-medical-admin/src/main/java/com/haoze/admin/service/impl/FastMenuServcieImpl.java
@@ -1,10 +1,17 @@
 package com.haoze.admin.service.impl;
 
+import com.haoze.admin.mapper.FastMenuMapper;
+import com.haoze.admin.mapper.MenuMapper;
 import com.haoze.admin.model.TFastMenu;
+import com.haoze.admin.model.TMenu;
 import com.haoze.admin.service.FastMenuServcie;
 import com.haoze.common.service.AbstractService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import tk.mybatis.mapper.entity.Condition;
+import tk.mybatis.mapper.entity.Example;
+
+import javax.annotation.Resource;
 
 /**
  * @author shenjun
@@ -13,4 +20,15 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class FastMenuServcieImpl extends AbstractService<TFastMenu> implements FastMenuServcie {
+    @Resource
+    private FastMenuMapper fastMenuMapper;
+
+    @Override
+    public void deleteById(Object id) {
+        Condition condition = new Condition(TFastMenu.class);
+        Example.Criteria criteria = condition.createCriteria();
+        criteria.andEqualTo("parentId", id);
+        fastMenuMapper.deleteByCondition(condition);
+        fastMenuMapper.deleteByPrimaryKey(id);
+    }
 }
