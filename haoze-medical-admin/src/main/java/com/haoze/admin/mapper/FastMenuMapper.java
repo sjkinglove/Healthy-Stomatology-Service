1f14d46c87ee9db90e2b7a475eee6a4afb9805be
shenjun <1024772494@qq.com>
2019-03-01 08:18:47 +0800

    OK

--- haoze-medical-admin/src/main/java/com/haoze/admin/mapper/FastMenuMapper.java
+++ haoze-medical-admin/src/main/java/com/haoze/admin/mapper/FastMenuMapper.java
@@ -3,6 +3,21 @@ package com.haoze.admin.mapper;
 import com.haoze.admin.model.TFastMenu;
 import com.haoze.common.mapper.MyMapper;
 
-public interface FastMenuMapper extends MyMapper<TFastMenu> {
+import java.util.List;
 
+public interface FastMenuMapper extends MyMapper<TFastMenu> {
+    /**
+     * 根据用户ID查询所有快捷通道
+     * */
+    List<TFastMenu> listByUserId(String userId);
+
+    /**
+     * 新增快捷通道
+     * */
+    void insertFastMenu(TFastMenu fastMenu);
+
+    /**
+     * 启用快捷通道
+     * */
+    void clearFastMenu(TFastMenu fastMenu);
 }
