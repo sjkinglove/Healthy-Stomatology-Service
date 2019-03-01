ac4693fe0827b37fd86c274ec0f8a055b87a7824
shenjun <1024772494@qq.com>
2019-03-01 17:10:59 +0800

    OK

--- haoze-medical-dental/src/main/java/com/haoze/dental/HaozeMedicalDentalApplication.java
+++ haoze-medical-dental/src/main/java/com/haoze/dental/HaozeMedicalDentalApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -25,6 +28,10 @@ public class HaozeMedicalDentalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HaozeMedicalDentalApplication.class, args);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
