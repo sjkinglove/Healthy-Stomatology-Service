6ae45cfd8abac25e8cbdfbbf47e833c437d8da3a
shenjun <1024772494@qq.com>
2019-03-04 18:59:25 +0800

    OK

--- haoze-medical-admin/src/main/java/com/haoze/admin/dto/system/OrganizationDTO.java
+++ haoze-medical-admin/src/main/java/com/haoze/admin/dto/system/OrganizationDTO.java
@@ -34,6 +34,8 @@ public class OrganizationDTO {
 
     private String phone;
 
+    private String userName;
+
 
     public String getToId() {
         return toId;
@@ -171,5 +173,13 @@ public class OrganizationDTO {
 
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 }
