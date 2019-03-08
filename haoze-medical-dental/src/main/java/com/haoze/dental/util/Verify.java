38ec37dd8caa94f742667d6ae297ccbe57390d9b
shenjun <1024772494@qq.com>
2019-03-08 16:50:32 +0800

    校验工具类

--- haoze-medical-dental/src/main/java/com/haoze/dental/util/Verify.java
+++ haoze-medical-dental/src/main/java/com/haoze/dental/util/Verify.java
@@ -3,6 +3,10 @@ package com.haoze.dental.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Verify {
     private final static Logger log = LoggerFactory.getLogger(Verify.class);
 
@@ -74,4 +78,49 @@ public class Verify {
         }
         return matches;
     }
+
+    /**
+     * 通过身份证号码获取出生日期、性别、年龄
+     * @param IDNumber
+     * @return 返回的出生日期格式：1990-01-01   性别格式：F-女，M-男
+     */
+    public static Map<String, String> getBirAgeSex(String IDNumber) {
+        String birthday = "";
+        String age = "";
+        String sexCode = "";
+
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        char[] number = IDNumber.toCharArray();
+        boolean flag = true;
+        if (number.length == 15) {
+            for (int x = 0; x < number.length; x++) {
+                if (!flag) return new HashMap<String, String>();
+                flag = Character.isDigit(number[x]);
+            }
+        } else if (number.length == 18) {
+            for (int x = 0; x < number.length - 1; x++) {
+                if (!flag) return new HashMap<String, String>();
+                flag = Character.isDigit(number[x]);
+            }
+        }
+        if (flag && IDNumber.length() == 15) {
+            birthday = "19" + IDNumber.substring(6, 8) + "-"
+                    + IDNumber.substring(8, 10) + "-"
+                    + IDNumber.substring(10, 12);
+            sexCode = Integer.parseInt(IDNumber.substring(IDNumber.length() - 3, IDNumber.length())) % 2 == 0 ? "F" : "M";
+            age = (year - Integer.parseInt("19" + IDNumber.substring(6, 8))) + "";
+        } else if (flag && IDNumber.length() == 18) {
+            birthday = IDNumber.substring(6, 10) + "-"
+                    + IDNumber.substring(10, 12) + "-"
+                    + IDNumber.substring(12, 14);
+            sexCode = Integer.parseInt(IDNumber.substring(IDNumber.length() - 4, IDNumber.length() - 1)) % 2 == 0 ? "F" : "M";
+            age = (year - Integer.parseInt(IDNumber.substring(6, 10))) + "";
+        }
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("birthday", birthday);
+        map.put("age", age);
+        map.put("sexCode", sexCode);
+        return map;
+    }
+
 }
